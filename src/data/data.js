b23e8bbf763a597f739d6550e55a546ed788bb40
kmelve <knut@netliferesearch.com>
2016-07-01 00:44:16 +0200

    add third article

--- src/data/data.js
+++ src/data/data.js
@@ -2,6 +2,7 @@
 const Data = [
     { id: 1, author: 'Pete Hunt', text: 'This is one comment' },
     { id: 2, author: 'Jordan Walke', text: 'This is *another* comment' },
+    { id: 3, author: 'Jordan Walke', text: 'This is *another* comment' }
 ];
 
 export default Data;
